17f6dc5130c6fb29ede48ac7a6fc4a5ef38bf7ab
Henrique Mendes <riquesmendes@gmail.com>
2023-02-02 12:41:32 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+
+import "./App.css";
+
+function getRandom() {
+ const random = (num) => Math.floor(Math.random() * num);
+
+document.getElementById('imgDog').src = "./dog" + random(10) + ".jpg";
+
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="align">
+        <button onClick={getRandom} className="button1"> Clique aqui</button>
+      </div>
+      <div className="dogImage">
+        <img id='imgDog' src={''} alt="Minha Figura"></img>
+      </div>
     </div>
   );
 }
---
+++ src/dogs.js
@@ -0,0 +1,47 @@
+const cachorros = [
+  {
+   
+    imagem: '../dog1.jpg'
+  },
+  {
+     
+      imagem: "../dog2.jpg"
+    },
+
+    {
+  
+      imagem: "../dog3.jpg"
+    },
+
+    {
+ 
+      imagem: "../dog4.jpg"
+    },
+
+    {
+  
+      imagem: "../dog5.jpg"
+    },
+
+    {
+   
+      imagem: "../dog6.jpg"
+    },
+
+    {
+     
+      imagem: "../dog7.jpg"
+    },
+
+    {
+  
+      imagem: "../dog8.jpg"
+    },
+
+    {
+  
+      imagem: "../dog9.jpg"
+    } 
+];
+
+export default cachorros;
